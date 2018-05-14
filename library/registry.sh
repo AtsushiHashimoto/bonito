4a317e9811bf9d4680bff4e70218dc6753ad4772
AtsushiHashimoto <ahasimoto@mm.media.kyoto-u.ac.jp>
2018-05-14 14:28:09 +0900

    take 1

--- library/registry.sh
+++ library/registry.sh
@@ -1,10 +1,11 @@
 
 function bonito_init_registry {
-  bonito_generate_certifications
+  mkdir -p $BONITO_
+  #bonito_generate_certifications
   docker pull $BONITO_DOCKER_REGISTRY_IMAGE
-  docker run --restart=always --name bonito_registry $BONITO_DOCKER_REGISTRY_IMAGE
+  docker run --restart=always -v $BONITO_REGIS_DIR:$BONITO_REGIS_DIR --name bonito_registry $BONITO_DOCKER_REGISTRY_IMAGE
 }
 
 function bonito_generate_certification {
-  
+  echo "hoge"
 }
